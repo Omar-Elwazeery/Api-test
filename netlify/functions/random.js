2cf5fea68745d497f409294b33b27143b6b100f4
Omar-Elwazeery <sim.omarahmed3077@alexu.edu.eg>
2025-03-07 16:12:55 +0200

    Update random.js

--- netlify/functions/random.js
+++ netlify/functions/random.js
@@ -1,14 +1,14 @@
 const sentences = [
-  "The quick brown fox jumps over the lazy dog.",
-  "Life is what happens when you're busy making other plans.",
-  "The only limit to our realization of tomorrow will be our doubts of today.",
-  "In three words I can sum up everything I've learned about life: it goes on.",
-  "The future belongs to those who believe in the beauty of their dreams.",
-  "Success is not final, failure is not fatal: It is the courage to continue that counts.",
-  "The best way to predict the future is to create it.",
-  "Happiness is not something ready-made. It comes from your own actions.",
-  "The purpose of our lives is to be happy.",
-  "You only live once, but if you do it right, once is enough."
+  "Hey $(user), did you know that the quick brown fox jumps over the lazy dog?",
+  "$(user) is making plans while life happens!",
+  "Don't let your doubts hold you back, $(user)!",
+  "Keep going $(user), life goes on!",
+  "$(user) believes in the beauty of their dreams.",
+  "Stay strong $(user), it's the courage to continue that counts!",
+  "$(user) is creating their own future!",
+  "$(user) knows that happiness comes from their own actions.",
+  "Hey $(user), the purpose of our lives is to be happy!",
+  "Make it count, $(user)!"
 ];
 
 exports.handler = async function(event, context) {
