2a8eb4cdbd7fe03d5e7506b85aa43ace5cc792f8
Omar-Elwazeery <sim.omarahmed3077@alexu.edu.eg>
2025-03-07 16:55:14 +0200

    welcome

--- netlify/functions/random.js
+++ netlify/functions/random.js
@@ -1,33 +1,33 @@
 const sentences = [
-  "أهلًا بالمسافر التائه بين الرمال، اجلس واسترح، وتفضل كأسًا من مشروب الصحاري الخطير، ودع الرياح تروي حكاياتها. 🍵 🌵",
-  "2جملة عشوائية",
-  "10جملة عشوائية"
-];
-
-
-exports.handler = async function(event, context) {
-  // Enable CORS
-  const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Content-Type': 'text/plain'
-  };
-
-  try {
-    // Get a random sentence
-    const randomIndex = Math.floor(Math.random() * sentences.length);
-    const randomSentence = sentences[randomIndex];
-
-    return {
-      statusCode: 200,
-      headers,
-      body: randomSentence
+    "جملة عشوائية",
+    "2جملة عشوائية",
+    "10جملة عشوائية"
+  ];
+  
+  
+  exports.handler = async function(event, context) {
+    // Enable CORS
+    const headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Content-Type': 'text/plain'
     };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      headers,
-      body: 'Error getting random sentence'
-    };
-  }
-}; 
+  
+    try {
+      // Get a random sentence
+      const randomIndex = Math.floor(Math.random() * sentences.length);
+      const randomSentence = sentences[randomIndex];
+  
+      return {
+        statusCode: 200,
+        headers,
+        body: randomSentence
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        headers,
+        body: 'Error getting random sentence'
+      };
+    }
+  }; 
